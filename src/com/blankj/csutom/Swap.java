b7e5c55a55f2be2245c72c67c9943ed12f5aff42
laileon <leon@igola.com>
2017-11-29 00:32:36 +0800

    交换两个数

---
+++ src/com/blankj/csutom/Swap.java
@@ -0,0 +1,12 @@
+package com.blankj.csutom;
+
+public class Swap {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+        System.out.printf("After swapping, 'x': %d, 'y': %d", x, y);
+    }
+}
