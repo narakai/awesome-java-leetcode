c71ba77082fe4c2dff142add4262e1e51d5e7238
laileon <leon@igola.com>
2017-11-30 11:43:42 +0800

    refactor code

--- src/com/blankj/csutom/Permutation.java
+++ src/com/blankj/csutom/Permutation.java
@@ -10,26 +10,23 @@ public class Permutation {
         System.out.println(Arrays.toString(permutation("abc")));
     }
 
-    private static String[] permutation(String orginal){
+    private static String[] permutation(String orginal) {
         System.out.println("orginal = " + orginal);
         ArrayList list = new ArrayList();
-        if(orginal.length() == 1){
+        if (orginal.length() == 1) {
             System.out.println("跳出递归 " + orginal);
             return new String[]{orginal};
-        } else{
+        } else {
             for (int i = 0; i < orginal.length(); i++) {
                 String s = orginal.charAt(i) + "";
-                String result = "";
-                String resultA = result + s;
-                System.out.println("resultA: " + resultA + " + " + i);
+                System.out.println("s: " + s + " + " + i);
                 //e.g bc时分别输出c b
                 String leftS = orginal.substring(0, i) + orginal.substring(i + 1, orginal.length());
                 System.out.println("leftS: " + leftS);
                 //递归 permutation(leftS)
                 for (String element : permutation(leftS)) {
-                    result = resultA + element;
-                    System.out.println("递归： " + resultA + " + " + element + " = " + result);
-                    list.add(result);
+                    System.out.println("递归： " + s + " + " + element + " = " + (s + element));
+                    list.add(s + element);
                 }
             }
             System.out.println(Arrays.toString(list.toArray()));
