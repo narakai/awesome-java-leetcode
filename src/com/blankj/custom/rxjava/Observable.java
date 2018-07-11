64a132d07bfedd971f512d5bc415051455d462f6
laileon <leon@igola.com>
2018-07-11 15:05:43 +0800

    rxjava like

--- src/com/blankj/custom/rxjava/Observable.java
+++ src/com/blankj/custom/rxjava/Observable.java
@@ -3,10 +3,13 @@ package com.blankj.custom.rxjava;
 public class Observable<T> {
     final OnSubscribe<T> onSubscribe;
 
+    //构造函数
     private Observable(OnSubscribe<T> onSubscribe) {
         this.onSubscribe = onSubscribe;
     }
 
+// 每个Observable里面有一个OnSubscribe对象，只有一个方法（void call(Subscriber<? super T> subscriber);），
+// 用来产生数据流，这是典型的命令模式。
     public static <T> Observable<T> create(OnSubscribe<T> onSubscribe) {
         return new Observable<T>(onSubscribe);
     }
