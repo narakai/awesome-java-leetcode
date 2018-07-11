4daa6b4dcbd36c73da00a0d077103ffc9eb7299e
laileon <leon@igola.com>
2018-07-11 16:37:04 +0800

    rxjava test

--- src/com/blankj/custom/rxjava/Observable.java
+++ src/com/blankj/custom/rxjava/Observable.java
@@ -1,6 +1,13 @@
 package com.blankj.custom.rxjava;
 
 public class Observable<T> {
+    //接口，每个Observable里面有一个OnSubscribe对象，只有一个方法（void call(Subscriber<? super T> subscriber);），
+    // 用来产生数据流，这是典型的命令模式。
+    public interface OnSubscribe<T> {
+        void call(Subscriber<? super T> subscriber);
+    }
+
+    //接口实现类
     final OnSubscribe<T> onSubscribe;
 
     //构造函数
@@ -8,21 +15,15 @@ public class Observable<T> {
         this.onSubscribe = onSubscribe;
     }
 
-// 每个Observable里面有一个OnSubscribe对象，只有一个方法（void call(Subscriber<? super T> subscriber);），
-// 用来产生数据流，这是典型的命令模式。
-
-//  <T>  告诉编译器我这个地方有个类型
+    //  <T>  告诉编译器我这个地方有个类型
     public static <T> Observable<T> create(OnSubscribe<T> onSubscribe) {
         return new Observable<T>(onSubscribe);
     }
 
     public void subscribe(Subscriber<? super T> subscriber) {
+        System.out.println("will call subscriber");
         subscriber.onStart();
+        //onSubscribe来call这个subscriber
         onSubscribe.call(subscriber);
     }
-
-    public interface OnSubscribe<T> {
-        void call(Subscriber<? super T> subscriber);
-    }
-
 }
--- src/com/blankj/custom/rxjava/Test.java
+++ src/com/blankj/custom/rxjava/Test.java
@@ -3,21 +3,28 @@ package com.blankj.custom.rxjava;
 public class Test {
     public static void main(String[] args) {
         Observable.create(new Observable.OnSubscribe<Integer>() {
+            //实例化接口实现类，需要override
             @Override
             public void call(Subscriber<? super Integer> subscriber) {
+                System.out.println("call method execute");
                 for (int i = 0; i < 10; i++) {
+                    System.out.println("Prepare data " + i);
                     subscriber.onNext(i);
                 }
+                subscriber.onCompleted();
             }
         }).subscribe(new Subscriber<Integer>() {
+            //实例化接口实现类，需要override
             @Override
             public void onCompleted() {
-
+                System.out.println("onCompleted");
             }
+
             @Override
             public void onError(Throwable t) {
 
             }
+
             @Override
             public void onNext(Integer var1) {
                 System.out.println(var1);
